e38b6ee43410ecbe9886391514e4bdadd88940db
rohitmanekar <rohitmanekar.work@gmail.com>
2024-07-08 18:17:31 +0100

    refactored api tests and used new api lib

--- src/main/api/apiHelper.ts
+++ src/main/api/apiHelper.ts
@@ -1,36 +1,56 @@
-import axios from 'axios';
+const request = require('request-promise-native');
 
+// Executing post request
 export async function executeRpcPost(apiUrl: string, method: string, params: any[]) {
-  console.log('Executing API:', apiUrl);
-
-  const headers = {
-    'accept': 'application/json',
-    'content-type': 'application/json',
+  console.log('Executing POST API:', apiUrl);
+  const options = {
+    method: 'POST',
+    uri: apiUrl,
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+    },
+    body: {
+      jsonrpc: '2.0',
+      id: 1,
+      method,
+      params,
+    },
+    json: true,
+    resolveWithFullResponse: true,
+    simple: false, // Prevents request from throwing on 4xx and 5xx statuses
   };
-  const data = {
-    jsonrpc: '2.0',
-    id: 1,
-    method,
-    params,
-  };
-
   try {
-    const response = await axios.post(apiUrl, data, { headers });
-    console.log('Response data:', response.data);
-    return response.data;
+    const response = await request(options);
+    console.log(response.body);
+    return {
+      status: response.statusCode,
+      body: response.body,
+    };
   } catch (error) {
     console.error('Error making the API request:', error);
     throw error;
   }
 }
 
+// Executing get request
 export async function executeRpcGet(apiUrl: string, headers = {}) {
   console.log('Executing GET API:', apiUrl);
-
+  const options = {
+    method: 'GET',
+    uri: apiUrl,
+    headers: headers,
+    json: true,
+    resolveWithFullResponse: true,
+    simple: false, 
+  };
   try {
-    const response = await axios.get(apiUrl, { headers });
-    console.log('GET Response data:', response.data);
-    return response.data;
+    const response = await request(options);
+    console.log(response.body);
+    return {
+      status: response.statusCode,
+      body: response.body,
+    };
   } catch (error) {
     console.error('Error making the API request:', error);
     throw error;
